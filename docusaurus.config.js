993206ab11b1b955a87261bc30f6842a7743ce54
James Kerrane <hi@jameskerrane.com>
2023-08-13 16:23:33 -0600

    🫰 Add GitHub Pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'thatrobotdev', // Usually your GitHub org/user name.
   projectName: 'theorybear', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
