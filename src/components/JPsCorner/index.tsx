4d03326e80093872fc7b30b0fadaf938ff8e7b3a
James Kerrane <hi@jameskerrane.com>
2023-10-16 19:24:08 +0000

    Update Jp's Corner message

--- src/components/JPsCorner/index.tsx
+++ src/components/JPsCorner/index.tsx
@@ -8,7 +8,7 @@ export default function JPsCorner(): JSX.Element {
             <br />
             <h2>JP's Corner ʕ •ᴥ•ʔ</h2>
             <p><i>Hello! I'm JP, the choir TA! In this corner of the site, I'll post updates on what I'm working on!</i></p>
-            <p><b>Currently working on:</b> Adding Alfred's Book 2 and Book 3 materials!</p>
+            <p><b>Currently working on:</b> Making the site easier to contribute to!</p>
             <p>I'm so excited to be working with you this year! If you have any questions or feedback, please email me at <a href="mailto:jpkerrane01@bvsd.org">jpkerrane01@bvsd.org</a> or contact Mr. Henning!</p>
         </div>
         </section>  
